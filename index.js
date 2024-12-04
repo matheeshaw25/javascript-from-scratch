abbadc390c6c3bf56fae06ebddf553dc559c9366
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-12-04 14:02:46 +0530

    Learned to display variables on the index.html page

--- index.js
+++ index.js
@@ -2,29 +2,11 @@
 // 1. Declaration let x;
 // 2. Assignment x=100;
 
-// Number
+let fullName = "Mat Wij";
+let age = 22;
+let isStudent = false;
 
-// let age = 25;
-// let price = 10.99;
-// let gpa = 2.1;
 
-// String
-
-// let first_name = "Mat";
-// let favoriteFood = "pizza";
-// let email = "mat123@gmail.com";
-
-// console.log(typeof first_name);
-// console.log(`Your name is ${first_name}`);
-// console.log(`You like ${favoriteFood}`);
-// console.log(`Your email is ${email}`);
-
-// Boolean
-
-let online = false;
-let forSale = true;
-let isStudent = true;
-
-console.log(`Bro is online: ${online}`);
-console.log(`Is this car for sale: ${forSale}`);
-console.log(`Enrolled: ${isStudent}`);
+document.getElementById("p1").textContent = `Your full name is ${fullName}`;
+document.getElementById("p2").textContent = `Your are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
