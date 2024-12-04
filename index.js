d65f6cf5901ac76c25289b2c70ff9a4f7bcadfa9
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-12-04 13:34:17 +0530

    Add initial structure with index.html, style.css, and index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log(`Hello`); 
+// console.log(`I like pizza`); // display a message on console
+
+// window.alert(`This is an alert!`); //display an alert
+// window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizzza!`;
+
+/*
+This
+is 
+a 
+comment
+*/
